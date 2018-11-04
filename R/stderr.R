7384a6c7f126ee168e249085795954c855dcc6e9
Conor Neilson <condwanaland@gmail.com>
2018-11-04 12:01:37 +0000

    add std_error function to remove plotrix dependency

---
+++ R/stderr.R
@@ -0,0 +1,20 @@
+#' std_error
+#'
+#' @param x A vector of numeric values
+#'
+#' @return Vector of length one
+#' @export
+#'
+#' @examples
+#' vec <- c(2, 3, 4, 5)
+#' std_error(vec)
+#'
+#' library(dplyr)
+#' data(iris)
+#' summarise(iris, petal_stderr = std_error(Petal.Length))
+
+std_error <- function(x){
+  sd(x, na.rm = TRUE)/sqrt(length(na.omit(x)))
+}
+
+
