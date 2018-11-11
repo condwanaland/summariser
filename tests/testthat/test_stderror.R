dc05a39704ce945ff5cd5ace14371a7b77921b27
Conor Neilson <condwanaland@gmail.com>
2018-11-11 18:50:29 +0000

    add more stderror tests

--- tests/testthat/test_stderror.R
+++ tests/testthat/test_stderror.R
@@ -1,9 +1,19 @@
 context("Testing std_error function")
 
-vec <- c(2, 3)
+vec1 <- c(5)
+vec2 <- c(2, 3)
 
 test_that("throws error when too few observations", {
-  expect_error(std_error(vec),
+  expect_error(std_error(vec1),
                "There must be at least three observations to compute a standard error",
                fixed = TRUE)
+  expect_error(std_error(vec2),
+               "There must be at least three observations to compute a standard error",
+               fixed = TRUE)
+})
+
+vec3 <- c(5, 10, 9, 7, 15)
+
+test_that("Computes std error correctly", {
+  expect_equal(std_error(vec3), 1.6853)
 })
