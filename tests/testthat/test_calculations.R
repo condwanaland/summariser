cf0f8db938997d7b322cbcf3b116318dc1174bf3
Conor Neilson <condwanaland@gmail.com>
2019-01-03 22:36:31 +0000

    Add test data. Update tests

--- tests/testthat/test_calculations.R
+++ tests/testthat/test_calculations.R
@@ -1,6 +1,6 @@
 context("Testing that correct numbers are returned")
 
-ungrouped_df <- readRDS(base::system.file("tests", "testdata",
+ungrouped_df <- readRDS(system.file("extdata",
                                           "ungrouped_iris.rds",
                                           package = "summariser"))
 
@@ -10,31 +10,51 @@ test_that("Ungrouped dfs are correctly calculated", {
   expect_equivalent(ungrouped_df, pkg_ungrouped_df)
 })
 
-
-grouped_df <- readRDS(base::system.file("tests", "testdata",
-                                          "grouped_iris.rds",
-                                          package = "summariser"))
-
-pkg_grouped_df <- summary_stats(iris, Sepal.Length, Species)
-
-test_that("Grouped dfs are correctly calculated", {
-  expect_equivalent(grouped_df, pkg_grouped_df)
+test_that("Ungrouped dfs have the right dims", {
+  expect_equal(dim(ungrouped_df), dim(pkg_ungrouped_df))
 })
 
 
-triple_grouped_df <- readRDS(base::system.file("tests", "testdata",
+group_df <- readRDS(system.file("extdata",
+                                          "grouped_iris.rds",
+                                          package = "summariser"))
+
+pkg_group_df <- summary_stats(iris, Sepal.Length, Species)
+
+test_that("Grouped dfs are correctly calculated", {
+  expect_equivalent(group_df[, 1], pkg_group_df[, 1])
+  expect_equivalent(group_df[, 2], pkg_group_df[, 2])
+  expect_equivalent(group_df[, 4], pkg_group_df[, 4])
+  expect_equivalent(group_df[, 5], pkg_group_df[, 5])
+})
+
+test_that("Grouped dfs have the right dimensions", {
+  expect_equal(dim(group_df), dim(pkg_group_df))
+})
+
+
+triple_grouped_df <- readRDS(system.file("extdata",
                                           "triple_grouped_mtcars.rds",
                                           package = "summariser"))
 
 pkg_triple_grouped_df <- summary_stats(mtcars, hp, am, gear, carb)
 
 test_that("Three levels of grouping calculates correctly", {
-  expect_equivalent(triple_grouped_df, pkg_triple_grouped_df)
+  expect_equivalent(triple_grouped_df[, 1], pkg_triple_grouped_df[, 1])
+  expect_equivalent(triple_grouped_df[, 2], pkg_triple_grouped_df[, 2])
+  expect_equivalent(triple_grouped_df[, 4], pkg_triple_grouped_df[, 4])
+  expect_equivalent(na.omit(triple_grouped_df[, 5]),
+                    na.omit(pkg_triple_grouped_df[, 5]))
+
+})
+
+test_that("Triple grouping has the right dims", {
+  expect_equal(dim(triple_grouped_df), dim(pkg_triple_grouped_df))
 })
 
 rm(ungrouped_df)
 rm(pkg_ungrouped_df)
-rm(grouped_df)
-rm(pkg_grouped_df)
+rm(group_df)
+rm(pkg_group_df)
 rm(triple_grouped_df)
 rm(pkg_triple_grouped_df)
